96964b30b737aec7bb6775a4b7aabed855e358f0
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2022-10-26 15:22:21 +0300

    Update script.py
    Products, Asins works only

--- script.py
+++ script.py
@@ -39,40 +39,44 @@ def searchReviews(reviewLink):
     return page
 
 
-#query = "graphics+cards"
-query = "ps5"
+query = "graphics+cards"
+#query = "ps5"
 #url = amazon_url + query
 
 #Without the header amazon will not give me access
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.91 Safari/537.36 ',
-          'referer': 'https://www.amazon.com/s?k=ps5&crid=MFZYID64FVKA&sprefix=ps%2Caps%2C605&ref=nb_sb_noss_1'}
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.93 Safari/537.36 ',
+          'referer': 'https://www.amazon.com/s?k=graphics+cards&ref=nb_sb_noss'}
 
-#https://www.amazon.com/s?k=graphics+cards&ref=nb_sb_noss
-page = getAmazonSearch(query)
-soup = BeautifulSoup(page.content, "lxml")
+#
 
 
 
 
+#&page=4&qid=1666777740&ref=sr_pg_4
 #extract the asin numbers 
 asins = []
-for i in soup.findAll("div", {'class': 's-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16'}):
-    asins.append(i['data-asin'])
+for j in range(3):
+    page = getAmazonSearch(query + '&page='+ str(j) + '&qid=1666777740&ref=sr_pg_'+ str(j))
+    soup = BeautifulSoup(page.content, "lxml") 
+    for i in soup.findAll("div", {'class': 's-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16'}):
+        asins.append(i['data-asin'])
     
 
 #call the getProductNames and store the names in a list
-product_names = []
+products = []
 for i in range(0, len(asins)):
-    product_names.append(getProductNames(asins[i]))
+    products.append(getProductNames(asins[i]))
     
-links = []
+'''    
+links = {}
 for i in range(len(asins)):
     page = searchAsin(asins[i])
     soup = BeautifulSoup(page.content, "lxml")
-    for j in soup.findAll("a", {'data-hook': "see-all-reviews-link-foot"}):
-        links.append(j['href'])
+    for j in soup.find("a", {'data-hook': "see-all-reviews-link-foot"}):
+        links[asins[i]] = j['href']
+    '''    
         
-
+'''
 
 reviews = []
 for i in range(len(links)):
@@ -81,9 +85,9 @@ for i in range(len(links)):
         soup = BeautifulSoup(page.content, "lxml")
         for k in soup.findAll("span", {'data-hook' : 'review-body'}):
             reviews.append(k.text)
+'''
 
-
-
+#p_a_df = pd.DataFrame({'data-asin':asins, 'product':products})
 
 
 
