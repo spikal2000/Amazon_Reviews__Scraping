ebe59a893d0cfaf7280198683e034214301f7c4b
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2022-10-29 10:33:54 +0300

    Updates
    fixing some minor bugs, and adding dataframes with asins, products and asins, reviews.
    ,Export pandas DataFrames to csv files
    products.csv,  reviews.csv

--- script.py
+++ script.py
@@ -39,17 +39,17 @@ def searchReviews(reviewLink):
     return page
 
 
+#query = "graphics+cards"
 query = "graphics+cards"
-#query = "ps5"
 #url = amazon_url + query
 
 #Without the header amazon will not give me access
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.20 Safari/537.36 ',
-          'referer': 'https://www.amazon.com/s?k=graphics+cards&ref=nb_sb_noss'}
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.94 Safari/537.36 ',
+          'referer': 'https://www.amazon.com/s?k=graphics+cards&crid=3B1INJ90VDLEB&sprefix=graphics+cards%2Caps%2C448&ref=nb_sb_noss_1'}
 
-#
+#https://www.amazon.com/s?k=iphone&crid=399XGN9Z3Y6UG&sprefix=ipho%2Caps%2C336&ref=nb_sb_noss_2
 
-
+##GPU https://www.amazon.com/s?k=graphics+cards&ref=nb_sb_noss
 
 
 #&page=4&qid=1666777740&ref=sr_pg_4
@@ -62,10 +62,7 @@ for j in range(3):
         asins.append(i['data-asin'])
     
 
-#call the getProductNames and store the names in a list
-products = []
-for i in range(0, len(asins)):
-    products.append(getProductNames(asins[i]))
+
     
     
 links = {}
@@ -90,12 +87,31 @@ for key, value in links.items():
             else:
                 reviews[key].append(k.text)
 
+#call the getProductNames and store the names in a list
+products = {}
+for p in reviews.keys():
+    #products.append(getProductNames(i))
+    products[p] = getProductNames(p)
+
+
+#reviews_2 = {}
+#reviews_df = pd.DataFrame()
+keys = []
+values = []
+for asin in reviews.keys():
+    for rreviews in reviews.values():
+        for _review in rreviews:
+            keys.append(asin)
+            values.append(_review)
+    
+        
 
 #p_a_df = pd.DataFrame({'data-asin':asins, 'product':products})
+products_df = pd.DataFrame({'asins': products.keys(), 'products': products.values()})
+reviews_df = pd.DataFrame({'asin':keys, 'product':values})
 
-
-
-
+products_df.to_csv('products.csv') #comma seperation
+reviews_df.to_csv('reviews.csv')
 
 
 
